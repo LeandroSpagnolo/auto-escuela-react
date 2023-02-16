efea88eccf800f101c35a214637a556f0ddbd494
Leandro Spagnolo <leandrospagnolo5@gmail.com>
2023-02-16 16:48:36 -0300

    transferida la bd de preguntas

--- src/components/Prueba.js
+++ src/components/Prueba.js
@@ -1,46 +1,17 @@
 import React, { useState } from 'react'
 import { VscDebugRestart } from 'react-icons/vsc'
+import preguntas from '../db/preguntas.json'
+
+preguntas = preguntas.lista
+
 
 function Prueba() {
   const [mostrarResultados, setMostrarResultados] = useState(false);
   const [pregutaActual, setPreguntaActual] = useState(0);
   const [puntaje, setPuntaje] = useState(0);
 
-  const preguntas = [
-    {
-      text: "Si circulo en una ruta detrás de un camión, y éste hace señas manuales y/o con la luz de giro derecha para que lo pase, ¿qué se debe hacer?",
-      options: [
-        { id: 0, text: "Inicio el adelantamiento con total seguridad.", isCorrect: false },
-        { id: 1, text: "Espero en mi carril hasta que el camión encienda la luz de giro izquierda.", isCorrect: false },
-        { id: 2, text: "Quedarse en el carril hasta que con los propios ojos se observa el camino y se comprueba que se tiene la vía libre en una distancia suficiente para evitar todo riesgo al sobrepasar.", isCorrect: true },
-      ],
-    },
-    {
-      text: "¿Qué indican las señales horizontales de líneas longitudinales continuas?",
-      options: [
-        { id: 0, text: "Sólo si son de color amarillo, indican que no debe ser traspasada ni se puede circular sobre ellas.", isCorrect: true },
-        { id: 1, text: "Independientemente del color, amarillo o blanco, indican que no debe ser traspasada ni se puede circular sobre ellas.", isCorrect: false },
-        { id: 2, text: "Indican los carriles o zonas de circulación.", isCorrect: false },
-      ],
-    },
-    {
-      text: "¿Cuál es la velocidad máxima en ciudad para un automóvil en calles?",
-      options: [
-        { id: 0, text: "40km/h.", isCorrect: true },
-        { id: 1, text: "50km/h.", isCorrect: false },
-        { id: 2, text: "60km/h.", isCorrect: false },
-      ],
-    },
-    {
-      text: "Una pick-up traslada fardos de pasto, traslada carga:",
-      options: [
-        { id: 0, text: "Liviana.", isCorrect: true },
-        { id: 1, text: "General.", isCorrect: false },
-        { id: 2, text: "Volátil.", isCorrect: false },
-      ],
-    },
-  ];
-
+  
+  
   const opcionClickeada = (esCorrecto) => {
     if (esCorrecto) {
       setPuntaje(puntaje + 1);
@@ -52,6 +23,8 @@ function Prueba() {
       setMostrarResultados(true);
     }
   };
+  
+  
 
 
   const reiniciarJuego = () => {
